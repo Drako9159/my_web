9d6e00d87b500ac5ba6cdfcc3f9afd7c85838d95
drako9159 <drako9159@gmail.com>
2022-10-29 02:19:02 -0500

    add new item

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,8 @@ import styles from "./Projects.module.css";
 
 import src0 from "../assets/img/rickAndMorty.png";
 import src1 from "../assets/img/container.astro.png";
+import src2 from "../assets/img/dr-web.png";
+import src3 from "../assets/img/traduction.png";
 
 const projects = [
   {
@@ -13,17 +15,24 @@ const projects = [
   },
   {
     id: 2,
-    name: "Astro Container",
+    name: "Astro",
     description: "Best market",
     url: "https://containers-pied.vercel.app/",
     src: src1,
   },
   {
     id: 3,
-    name: "Timer",
-    description: "consult your climate",
+    name: "Web traslate",
+    description: "website complete",
     url: "none",
-    src: src0,
+    src: src3,
+  },
+  {
+    id: 4,
+    name: "Dr-web",
+    description: "Complete webiste for a pediatrician",
+    url: "https://dr-web.vercel.app/",
+    src: src2,
   },
 ];
 const test = projects[0].src;
